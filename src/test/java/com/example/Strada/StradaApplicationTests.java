cfc081e5ed25482bc1bbacd67dae3f51773b88f6
Camil458 <k.radlowski458@gmail.com>
2023-02-25 19:56:03 +0100

    Initial commit

---
+++ src/test/java/com/example/Strada/StradaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Strada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
